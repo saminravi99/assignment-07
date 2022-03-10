b185f392a6eece59ec3d3f2869b8e38419feeaab
Samin Israr Ravi <saminisrar1@gmail.com>
2022-03-11 00:46:07 +0600

    error handled

--- js/app.js
+++ js/app.js
@@ -36,10 +36,12 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "accordion-section").style.display = "block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "accordion-section").style.display = "none";
         document.getElementById( "liked" ).innerHTML = "";
 
         displayLikedPosts();
@@ -48,6 +50,7 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).innerHTML = "";
+        document.getElementById( "accordion-section").style.display = "none";
         displayReportedPosts();
     }
 };
